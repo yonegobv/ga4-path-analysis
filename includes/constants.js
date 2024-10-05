8acd99a06e678e1e652a72c2d5192ffe51108bb3
Nico Brooks <nico@twooctobers.com>
2024-10-05 18:39:43 +0000

    First commit from dataform

---
+++ includes/constants.js
@@ -0,0 +1,10 @@
+
+const SOURCE_DATABASE = "twooctobersbigquery"; // the database containing the GA4 BigQuery exports
+const SOURCE_DATASET = "analytics_250617373"; // the dataset containing the GA4 BigQuery exports
+const OUTPUT_DATASET = "sandbox"; 
+
+module.exports = {
+    SOURCE_DATABASE,
+    SOURCE_DATASET,
+    OUTPUT_DATASET,
+};
