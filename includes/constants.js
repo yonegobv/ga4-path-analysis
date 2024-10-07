3f55099ec3452ad0b85f621528095e5196618448
Nico Brooks <nico@twooctobers.com>
2024-10-07 10:55:04 -0600

    Update constants.js

--- includes/constants.js
+++ includes/constants.js
@@ -1,10 +1,10 @@
 
-const SOURCE_DATABASE = "twooctobersbigquery"; // the database containing the GA4 BigQuery exports
-const SOURCE_DATASET = "analytics_250617373"; // the dataset containing the GA4 BigQuery exports
+const SOURCE_DATABASE = "your_database"; // the database containing the GA4 BigQuery exports
+const SOURCE_DATASET = "analytics_12345678"; // the dataset containing the GA4 BigQuery exports
 const OUTPUT_DATASET = "sandbox"; 
 
 module.exports = {
     SOURCE_DATABASE,
     SOURCE_DATASET,
-    OUTPUT_DATASET,
+    OUTPUT_DATASET
 };
